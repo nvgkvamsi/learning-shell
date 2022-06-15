dbadf9f6099cd1edab280818cbb3c75aa98c710b
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-06-15 14:07:55 -0700

    printing in shell

--- 02-print.sh
+++ 02-print.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+# To print some message on screen then
+# Command : echo
+# syntax: echo INPUT
+
 
 echo Hello World
 
+ # print multiples lines
 
-echo -e "hello,\nhow are you"
+echo -e "hello,\nhow are you \nGud Morning"
+
+# print letters with tabspace
 
 echo -e "helo\tvamsi"
 

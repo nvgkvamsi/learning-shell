5a3020d9f148447a133a1124cbc7e1dae0a25b52
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-06-15 14:18:56 -0700

    printing in shell

--- 02-print.sh
+++ 02-print.sh
@@ -8,10 +8,27 @@ echo Hello World
 
  # print multiples lines
 
-echo -e "hello,\nhow are you \nGud Morning"
+echo -e "hello\nhow are you \nGud Morning"
 
 # print letters with tabspace
 
-echo -e "helo\tvamsi"
+echo -e "hello\tvamsi"
 
-echo -e "\e[31mHelo in RED COllor\e[0m"
+# print some colors
+# syntax : echo -e "\e[COLmMessage\e[0m"
+# -e -> enable esc seq
+# \e -> enable col
+# [COLm -> Give color input
+#\e [0m -> Disable the color
+
+# The colors we have are:
+# COL      CODE            SYNTAX
+#RED        31            \e[31m
+#GREEN      32            \e[32m
+#YELLOW     33            \e[33m
+#BLUE       34            \e[34m
+#MAGENTA    35            \e[35m
+#CYAN       36            \e[36m
+
+echo -e "\e[31mHello in RED Color\e[0m"
+echo "Hello in Normal Color"

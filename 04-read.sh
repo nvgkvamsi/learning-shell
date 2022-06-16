53f4e041e0ac289424169bf37fe5c049d210a834
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-06-16 14:10:26 -0700

    variables demo

---
+++ 04-read.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+read -p 'Enter your name:' name
+echo -e "Hey $name, Welcome to DevOps Training..."
+
+read -p "Enter your mobile number:" phone
+echo "Thank you for sharing your mobile number" $phone
